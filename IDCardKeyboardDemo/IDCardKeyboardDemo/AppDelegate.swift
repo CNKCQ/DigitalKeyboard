668029abf83e972f1895bb94a129402dfa0a95bf
KingCQ <wangchengqvan@hotmail.com>
2016-06-28 12:38:34 +0800

    create

---
+++ IDCardKeyboardDemo/IDCardKeyboardDemo/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  IDCardKeyboardDemo
+//
+//  Created by KingCQ on 16/5/30.
+//  Copyright © 2016年 KingCQ. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    func applicationDidFinishLaunching(_ application: UIApplication) {
+        
+    }
+
+
+
+}
+//创建cocoapods
+//http://chinafishnews.github.io/2016/03/04/%E5%A6%82%E4%BD%95%E5%8F%91%E5%B8%83%E8%87%AA%E5%B7%B1%E7%9A%84%E5%BC%80%E6%BA%90%E6%A1%86%E6%9E%B6%E5%88%B0CocoaPods/
+
