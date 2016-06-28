d578877c8341087aa8cfe7342c2afb8697fc6c75
KingCQ <wangchengqvan@hotmail.com>
2016-06-28 12:05:33 +0800

    modify some code

--- IDCardKeyboardDemo/IDCardKeyboardDemo/ViewController.swift
+++ IDCardKeyboardDemo/IDCardKeyboardDemo/ViewController.swift
@@ -11,24 +11,32 @@ import UIKit
 class ViewController: UIViewController {
     let textField: UITextField = UITextField()
     let fieldNext: UITextField = UITextField()
+    var fields: [UITextField] = []
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .blueColor()
-        textField.frame = CGRect(x: 80, y: 400, width: 160, height: 30)
+        textField.frame = CGRect(x: 80, y: 400, width: 200, height: 30)
         textField.borderStyle = .RoundedRect
         textField.clearButtonMode = .WhileEditing
         textField.becomeFirstResponder()
         view.addSubview(textField)
         textField.keyboardType = .DecimalPad
-        fieldNext.frame = CGRect(x: 80, y: 500, width: 160, height: 30)
+        fieldNext.frame = CGRect(x: 80, y: 500, width: 200, height: 30)
         fieldNext.borderStyle = .RoundedRect
         fieldNext.placeholder = "nextField"
         view .addSubview(fieldNext)
         textField.becomeFirstResponder()
         textField.idcardKeyboard(view)//一句代码搞定数字键盘
 //        fieldNext.idcardKeyboard(view)
+        for idx in 1...3 {
+            let field = UITextField(frame: CGRect(x:idx * (80 - 2), y:180, width: 80, height: 40))
+            field.layer.borderWidth = 1
+            view.addSubview(field)
+            fields.append(field)
+        }
 
     }
 
