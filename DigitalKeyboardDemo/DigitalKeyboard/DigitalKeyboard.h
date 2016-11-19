3d81aba5dbd5c5bb0d2bac3e8d6e889323c66dd0
KingCQ <wangchengqvan@hotmail.com>
2016-11-19 16:51:38 +0800

    add bundle file

---
+++ DigitalKeyboardDemo/DigitalKeyboard/DigitalKeyboard.h
@@ -0,0 +1,19 @@
+//
+//  DigitalKeyboard.h
+//  DigitalKeyboard
+//
+//  Created by KingCQ on 2016/11/19.
+//  Copyright © 2016年 Jack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DigitalKeyboard.
+FOUNDATION_EXPORT double DigitalKeyboardVersionNumber;
+
+//! Project version string for DigitalKeyboard.
+FOUNDATION_EXPORT const unsigned char DigitalKeyboardVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DigitalKeyboard/PublicHeader.h>
+
+
